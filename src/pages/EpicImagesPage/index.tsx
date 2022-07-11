829fd696e7a6d90db526abe5a37317005a75dcef
d.sobolevskyi <d.sobolevskyi@viseven.com>
2022-07-11 17:54:26 +0300

    add(Pages): components

---
+++ src/pages/EpicImagesPage/index.tsx
@@ -0,0 +1,110 @@
+import { Button, Skeleton, Table, TableCell, TableHead, TableRow } from "@mui/material";
+import { Container } from "@mui/system";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import { getAllDates, getImageUrlByDateAndId, getNaturalImages, NeowsNaturalImagesRequestResponse } from "../../services/http/nasa/epic-camera";
+import Dialog from '@mui/material/Dialog';
+import PositionedSkeleton from "../../components/PositionedSkeleton/PositionedSkeleton";
+import router, {RouterPages} from "../../router";
+
+export default function EpicImagesPage() {
+  const tableHead = [
+    '',
+    'Caption',
+    'Version',
+    'Centroid Coordinates',
+    'Discover J2000 Position ',
+    'Lunar J2000 Position',
+    'Sun J2000 Position ',
+    'Attitude Quaternions ',
+    'Date'
+  ]
+
+  const [naturalImagesConfiguration, setNaturalImagesConfiguration] = useState<NeowsNaturalImagesRequestResponse[]>();
+  const [imageUrl, setImageUrl] = useState<string>('');
+  const [imageSelected, setImageSelected] = useState<boolean>(false);
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    getAllDates();
+    getNaturalImages()
+      .then((response) => setNaturalImagesConfiguration(response.data));
+      //.catch(/);
+  }, []);
+
+
+  const handleSelectImageClick = (value: NeowsNaturalImagesRequestResponse) => () => {
+    const date = moment(value.date);
+
+
+    setImageUrl(getImageUrlByDateAndId({
+      year: date.format('YYYY'),
+      month: date.format('MM'),
+      day: date.format('DD'),
+      imageId: value.image,
+    }));
+  
+    setImageSelected(true)
+  }
+
+  const handleImageModalClose = () => {
+    setImageSelected(false);
+    setImageUrl('');
+    setImageLoaded(false);
+  }
+
+  const handleImageLoad = () => {
+    console.log('image loaded');
+    setImageLoaded(true);
+  }
+
+  return (
+  <Container maxWidth={false} data-testid={router[RouterPages.epic].testId}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          {
+            tableHead.map((head, index) => (
+              <TableCell key={index}>
+                    { head }
+              </TableCell>
+            ))
+          }
+        </TableRow>
+      </TableHead>
+      {
+        naturalImagesConfiguration?.map((row: NeowsNaturalImagesRequestResponse, index) => (
+          <TableRow hover key={index}>
+            <TableCell>
+              <Button onClick={handleSelectImageClick(row)}>Watch Image</Button>
+            </TableCell>
+            <TableCell> {row.caption} </TableCell>
+            <TableCell> {row.version} </TableCell>
+            <TableCell> {`lat: ${row.centroid_coordinates.lat} <br /> lon: ${row.centroid_coordinates.lon}`} </TableCell>
+            <TableCell> {`x: ${row.dscovr_j2000_position.x} y: ${row.dscovr_j2000_position.y} z: ${row.dscovr_j2000_position.z}`} </TableCell>
+            <TableCell> {`x: ${row.lunar_j2000_position.x} y: ${row.lunar_j2000_position.y} z: ${row.lunar_j2000_position.z}`} </TableCell>
+            <TableCell> {`x: ${row.sun_j2000_position.x} y: ${row.sun_j2000_position.y} z: ${row.sun_j2000_position.z}`} </TableCell>
+            <TableCell> {`q0: ${row.attitude_quaternions.q0} q1: ${row.attitude_quaternions.q1} q2: ${row.attitude_quaternions.q2} q3: ${row.attitude_quaternions.q3}`} </TableCell>
+            <TableCell> {row.date} </TableCell>
+          </TableRow>
+        )) 
+      }
+    </Table>
+    <Dialog
+      open={imageSelected}
+      onClose={handleImageModalClose}
+    >
+      {
+        <>
+          <img onLoad={handleImageLoad} src={imageUrl} alt='Earth Image' style={{opacity: imageLoaded ? 1 : 0 }} />
+          {
+            imageLoaded ? <></> : <PositionedSkeleton position='absolute' top='0' left='0' animation="wave" variant="rectangular" width='100%' height='100%' />
+          }
+        </>
+        
+      }
+    </Dialog>
+
+  </Container>
+  );
+}
---
+++ src/pages/PictureOfTheDayPage/index.tsx
@@ -0,0 +1,69 @@
+import { Card, CardContent, Container, Skeleton, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import router, { RouterPages } from "../../router";
+import { APODGetPictureRequestResponse, getPictureOfTheDay } from "../../services/http/nasa/apod";
+
+export default function PictureOfTheDayPage() {
+  const [pictureOfTheDay, setPictureOfTheDay] = useState<APODGetPictureRequestResponse>();
+
+  useEffect(() => {
+    setTimeout(() => {
+      getPictureOfTheDay()
+        .then((response) => {
+          setPictureOfTheDay(response.data);
+        })
+    }, 2000);
+  }, [])
+
+
+  const getPicture = () => {
+    return pictureOfTheDay?.url ? <img src={pictureOfTheDay?.url} alt='picture of ther day' /> : undefined;
+  }
+
+  return (
+    <Container maxWidth={false} data-testid={router[RouterPages.pictureOfTheDay].testId}>
+      <Card 
+        sx={{
+          maxWidth: '768px',
+          margin: '10px auto',
+        }}
+      >
+        <CardContent>
+          {
+            !!pictureOfTheDay 
+              ? 
+                <Stack spacing={2}>
+                  {
+                    getPicture()
+                  }
+
+                  <Typography variant='h4'>
+                    {
+                      pictureOfTheDay?.title
+                    }
+                  </Typography>
+                  <Typography variant='body1'>
+                    {
+                      pictureOfTheDay?.explanation
+                    }
+                  </Typography>
+                  <Typography variant='body2'>
+                    {
+                      `Copyright ${pictureOfTheDay?.copyright} ${pictureOfTheDay?.date}`
+                    }
+                  </Typography>
+                </Stack>
+              : 
+                <Stack spacing={2}>
+                  <Skeleton animation="wave" variant="rectangular" width='100%' height={200} />
+                  <Skeleton animation="wave" variant="rectangular" width='100%' height={80} />
+                  <Skeleton animation="wave" variant="rectangular" width='100%' height={350} />
+                  <Skeleton animation="wave" variant="rectangular" width='100%' height={10} />
+                </Stack>
+
+          }
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
